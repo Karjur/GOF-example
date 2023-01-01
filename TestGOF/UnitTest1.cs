1b4b28288880ce6c3327146f392b84e76e5e8f5f
Polina Volohhonskaja <povolo@ttu.ee>
2023-01-01 23:59:14 +0200

    testid

--- TestGOF/UnitTest1.cs
+++ TestGOF/UnitTest1.cs
@@ -1,84 +1,99 @@
+using System.IO;
+
 namespace TestGOF
 {
     [TestClass]
     public class UnitTest1
     {
         // Test that the nurse can treat a patient with a health level within her range
-[TestMethod]
-public void TestNurseCanTreatPatient()
-{
-    // Create a nurse and set up the chain of responsibility
-    Nurse nurse = new Nurse();
-    Doctor doctor = new Doctor();
-    Surgeon surgeon = new Surgeon();
-    nurse.SetSuccessor(doctor);
-    doctor.SetSuccessor(surgeon);
+        [TestMethod]
+        public void TestNurseCanTreatPatient()
+        {
+            StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
 
-    // Create a patient with a health level within the nurse's range
-    Patient patient = new Patient(2);
+            // Create a nurse and set up the chain of responsibility
+            Nurse nurse = new Nurse();
+            Doctor doctor = new Doctor();
+            Surgeon surgeon = new Surgeon();
+            nurse.SetSuccessor(doctor);
+            doctor.SetSuccessor(surgeon);
 
-    // The nurse should be able to treat the patient
-    nurse.TreatPatient(patient);
-    Assert.AreEqual("Õde ravis patsiendi", Console.Out.ToString().Trim());
-}
+            // Create a patient with a health level within the nurse's range
+            Patient patient = new Patient(2);
 
-// Test that the doctor can treat a patient with a health level within her range
-[TestMethod]
-public void TestDoctorCanTreatPatient()
-{
-    // Create a nurse and set up the chain of responsibility
-    Nurse nurse = new Nurse();
-    Doctor doctor = new Doctor();
-    Surgeon surgeon = new Surgeon();
-    nurse.SetSuccessor(doctor);
-    doctor.SetSuccessor(surgeon);
+            // The nurse should be able to treat the patient
+            nurse.TreatPatient(patient);
+            Assert.AreEqual("Ã•de ravis patsiendi", stringWriter.ToString().Trim());
+        }
 
-    // Create a patient with a health level within the doctor's range
-    Patient patient = new Patient(5);
+        // Test that the doctor can treat a patient with a health level within her range
+        [TestMethod]
+        public void TestDoctorCanTreatPatient()
+        {
+            StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
 
-    // The doctor should be able to treat the patient
-    nurse.TreatPatient(patient);
-    Assert.AreEqual("Arst ravis patsiendi", Console.Out.ToString().Trim());
-}
+            // Create a nurse and set up the chain of responsibility
+            Nurse nurse = new Nurse();
+            Doctor doctor = new Doctor();
+            Surgeon surgeon = new Surgeon();
+            nurse.SetSuccessor(doctor);
+            doctor.SetSuccessor(surgeon);
 
-// Test that the surgeon can treat a patient with a health level within her range
-[TestMethod]
-public void TestSurgeonCanTreatPatient()
-{
-    // Create a nurse and set up the chain of responsibility
-    Nurse nurse = new Nurse();
-    Doctor doctor = new Doctor();
-    Surgeon surgeon = new Surgeon();
-    nurse.SetSuccessor(doctor);
-    doctor.SetSuccessor(surgeon);
+            // Create a patient with a health level within the doctor's range
+            Patient patient = new Patient(5);
 
-    // Create a patient with a health level within the surgeon's range
-    Patient patient = new Patient(9);
+            // The doctor should be able to treat the patient
+            nurse.TreatPatient(patient);
 
-    // The surgeon should be able to treat the patient
-    nurse.TreatPatient(patient);
-    Assert.AreEqual("Kirurg ravis patsiendi", Console.Out.ToString().Trim());
-}
+            Assert.AreEqual("Arst ravis patsiendi", stringWriter.ToString().Trim());
 
-// Test that no one can treat a patient with a health level outside the range of all medical professionals
-[TestMethod]
-public void TestNoOneCanTreatPatient()
-{
-    // Create a nurse and set up the chain of responsibility
-    Nurse nurse = new Nurse();
-    Doctor doctor = new Doctor();
-    Surgeon surgeon = new Surgeon();
-    nurse.SetSuccessor(doctor);
-    doctor.SetSuccessor(surgeon);
+        }
 
-    // Create a patient with a health level outside the range of all medical professionals
-    Patient patient = new Patient(12);
+        // Test that the surgeon can treat a patient with a health level within her range
+        [TestMethod]
+        public void TestSurgeonCanTreatPatient()
+        {
+            StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
 
-    // No one should be able to treat the patient
-    nurse.TreatPatient(patient);
-    string vastus = Console.ReadLine();
-    Assert.AreEqual("No one was able to treat the patient.", Console.Out.ToString().Trim());
-}
+            // Create a nurse and set up the chain of responsibility
+            Nurse nurse = new Nurse();
+            Doctor doctor = new Doctor();
+            Surgeon surgeon = new Surgeon();
+            nurse.SetSuccessor(doctor);
+            doctor.SetSuccessor(surgeon);
+
+            // Create a patient with a health level within the surgeon's range
+            Patient patient = new Patient(9);
+
+            // The surgeon should be able to treat the patient
+            nurse.TreatPatient(patient);
+            Assert.AreEqual("Kirurg ravis patsiendi", stringWriter.ToString().Trim());
+        }
+
+        // Test that no one can treat a patient with a health level outside the range of all medical professionals
+        [TestMethod]
+        public void TestNoOneCanTreatPatient()
+        {
+            StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
+
+            // Create a nurse and set up the chain of responsibility
+            Nurse nurse = new Nurse();
+            Doctor doctor = new Doctor();
+            Surgeon surgeon = new Surgeon();
+            nurse.SetSuccessor(doctor);
+            doctor.SetSuccessor(surgeon);
+
+            // Create a patient with a health level outside the range of all medical professionals
+            Patient patient = new Patient(12);
+
+            // No one should be able to treat the patient
+            nurse.TreatPatient(patient);
+            Assert.AreEqual("Keegi ei suuda enam patsienti ravida", stringWriter.ToString().Trim());
+        }
 
     }
 }
